6d4d68baf218fdeb11732c755e8054262d395485
Jose <josedcastro97@gmail.com>
2023-11-24 10:00:47 +0100

    feat: create splash screen

--- app/src/main/java/com/jdccmobile/costofliving/MainActivity.kt
+++ app/src/main/java/com/jdccmobile/costofliving/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.jdccmobile.costofliving
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        splashScreen.setKeepOnScreenCondition { false }
+
+        val intent = Intent(this, IntroActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
