fa5af591698aebebe25b22ede3d5cab158ce674f
JoseD <jdccMobile@gmail.com>
2024-06-15 13:16:50 +0200

    feat: add and configure ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,9 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.5.3" apply false
     id("io.gitlab.arturbosch.detekt") version "1.23.6"
+    id("org.jlleitschuh.gradle.ktlint") version "12.0.3"
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 }
