cb3368eff2092fe69333d7d86e62de1bede13871
JoseD <jdccMobile@gmail.com>
2024-07-23 08:26:19 +0200

    ref: fix error to string

--- app/src/main/java/com/jdccmobile/costofliving/common/Utils.kt
+++ app/src/main/java/com/jdccmobile/costofliving/common/Utils.kt
@@ -1,8 +1,10 @@
 package com.jdccmobile.costofliving.common
 
 import android.content.Context
+import androidx.sqlite.SQLiteException
 import com.jdccmobile.costofliving.App
 import com.jdccmobile.costofliving.R
+import retrofit2.HttpException
 import java.util.Locale
 
 val Context.app: App
@@ -15,8 +17,9 @@ fun getCountryCode(countryName: String) =
 
 @Suppress("MagicNumber")
 fun Throwable.toStringResource() = when {
-    message?.contains("429") == true -> R.string.http_429
-    this is retrofit2.HttpException && this.code() in 400..499 -> R.string.http_4xx_error
+    this is HttpException && this.code() == 429 -> R.string.http_429_error
+    this is HttpException && this.code() in 400..499 -> R.string.http_4xx_error
+    this is SQLiteException -> R.string.room_error
     this is androidx.datastore.core.IOException -> R.string.datastore_error
     else -> R.string.generic_error
 }
